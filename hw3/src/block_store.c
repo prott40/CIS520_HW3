9a5196ce2d24f83629ce641590b924e7861aa4d2
prott40 <143848245+prott40@users.noreply.github.com>
2025-03-11 13:44:18 -0500

    Update block_store.c
    complete checkpoint 2 and fix errors from cp1

--- hw3/src/block_store.c
+++ hw3/src/block_store.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
-
+#include <string.h>
+#include <stdbool.h>
 #include "bitmap.h"
 #include "block_store.h"
 // include more if you need
@@ -16,12 +17,23 @@
 ///
 block_store_t *block_store_create()
 {
-	block_store_t * b = (block_store_t*)malloc(sizeof(block_store_t));
+	block_store_t * b = (block_store_t *)calloc(1, sizeof(block_store_t));
 	if(b == NULL)
 	{
 		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
 		return NULL;
 	}
+	b->bitmap = bitmap_overlay(BLOCK_STORE_NUM_BLOCKS, b->block_data);
+    if (!b->bitmap) {
+        fprintf(stderr, "%s:%d Bitmap overlay creation failed\n", __FILE__, __LINE__);
+        free(b);
+        return NULL;
+    }
+
+    // Mark bitmap blocks as allocated
+    for (size_t i = 0; i < BITMAP_NUM_BLOCKS; ++i) {
+        block_store_request(b, BITMAP_START_BLOCK + i);
+    }
 	return b;
 }
 
@@ -33,47 +45,121 @@ block_store_t *block_store_create()
 void block_store_destroy(block_store_t *const bs)
 {
 	if(bs != NULL){
+		if(bs->bitmap!=NULL){
+			bitmap_destroy(bs->bitmap);
+		}
 		free(bs);
 	}
 	else{
 		fprintf(stderr, "%s:%d block is already free\n", __FILE__, __LINE__);
 	}
 }
-
+///
+/// Searches for a free block, marks it as in use, and returns the block's id
+/// \param bs BS device
+/// \return Allocated block's id, SIZE_MAX on error
+///
 size_t block_store_allocate(block_store_t *const bs)
 {
-	UNUSED(bs);
-	return 0;
+	// check parameters for null
+	if(!bs || !bs->bitmap){
+		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
+		return SIZE_MAX;
+	}
+	// get frist free memory
+	size_t free = bitmap_ffz(bs->bitmap);
+	// check if the location is outside of memoryu
+	if(free == SIZE_MAX || free >= BLOCK_STORE_NUM_BLOCKS){
+		return SIZE_MAX;
+	}
+	// set memmory
+	bitmap_set(bs->bitmap,free);
+	// return location
+	return free;
 }
-
+///
+/// Attempts to allocate the requested block id
+/// \param bs the block store object
+/// \block_id the requested block identifier
+/// \return boolean indicating succes of operation
+///
 bool block_store_request(block_store_t *const bs, const size_t block_id)
 {
-	UNUSED(bs);
-	UNUSED(block_id);
+	// check for null parameters
+	if(!bs|| !bs->bitmap){
+		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
+		return false;
+	}
+	// check for valid block id
+	if(block_id >= BLOCK_STORE_NUM_BLOCKS){
+		fprintf(stderr, "%s:%d block id is invalid\n", __FILE__, __LINE__);
+		return false;
+	}
+	// check for already allocated memory
+	if(bitmap_test(bs->bitmap,block_id)){
+		fprintf(stderr, "%s:%d block is already allocated\n", __FILE__, __LINE__);
+		return false;
+	}
+	// set the bitmap for new memory
+	bitmap_set(bs->bitmap, block_id);
+	// return the value if memory is set
+	if(bitmap_test(bs->bitmap,block_id)){
+		return true;
+	}
+	
 	return false;
 }
 
 void block_store_release(block_store_t *const bs, const size_t block_id)
 {
-	UNUSED(bs);
-	UNUSED(block_id);
+	// check for null parameters
+	if(!bs|| !bs->bitmap){
+		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
+		return;
+	}
+	// check for valid block id
+	if(block_id >= BLOCK_STORE_NUM_BLOCKS){
+		fprintf(stderr, "%s:%d block id is invalid\n", __FILE__, __LINE__);
+		return;
+	}
+	// reset value in the bitmap
+	bitmap_reset(bs->bitmap, block_id);
 }
-
+///
+/// Counts the number of blocks marked as in use
+/// \param bs BS device
+/// \return Total blocks in use, SIZE_MAX on error
+///
 size_t block_store_get_used_blocks(const block_store_t *const bs)
 {
-	UNUSED(bs);
-	return 0;
+	if(!bs || !bs->bitmap){
+		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
+		return SIZE_MAX;
+	}
+	return bitmap_total_set(bs->bitmap);
 }
-
+///
+/// Counts the number of blocks marked free for use
+/// \param bs BS device
+/// \return Total blocks free, SIZE_MAX on error
+///
 size_t block_store_get_free_blocks(const block_store_t *const bs)
 {
-	UNUSED(bs);
-	return 0;
+	if(!bs || !bs->bitmap){
+		fprintf(stderr, "%s:%d invalid parameters\n", __FILE__, __LINE__);
+		return SIZE_MAX;
+	}
+	size_t used = block_store_get_used_blocks(bs);
+	return BLOCK_STORE_NUM_BLOCKS - used;
 }
-
+///
+/// Returns the total number of user-addressable blocks
+///  (since this is constant, you don't even need the bs object)
+/// \return Total blocks
+///
 size_t block_store_get_total_blocks()
 {
-	return 0;
+	return BLOCK_STORE_NUM_BLOCKS;
 }
 
 size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
